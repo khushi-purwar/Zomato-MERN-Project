70157e54760cf128cc46190d30d502eff83162c5
Khushi Purwar <khushipurwar4@gmail.com>
2021-09-10 23:32:44 +0530

    add active tab to mobile screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import HomeLayoutHOC from "./HOC/Home.Hoc";
 
 // components
 import Temp from "./Components/Temp";
+import Master from "./Components/master";
+
 
 function App() {
   return (
     <>
      <HomeLayoutHOC path='/' exact component={Temp} />
+     <HomeLayoutHOC path="/:type" exact component={Master} />
     </>
   );
 }
