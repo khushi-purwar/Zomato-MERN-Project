6e0f21b2179a6a2ae37fff4fd35e029ab8b039b2
Khushi Purwar <khushipurwar4@gmail.com>
2021-09-11 18:32:59 +0530

    added nutrition carousel

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// components
+import NutritionCarousel from './NutritionCarousel';
+
+const Nutrition = () => {
+    return (
+        <div>
+            < NutritionCarousel />
+        </div>
+    )
+}
+
+export default Nutrition;
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom';
 import Delivery from './Delivery';
 import Dining from './Dining';
 import NightLife from './NightLife';
+import Nutrition from './Nutrition';
 
 const Master = () => {
     const {type} = useParams();
@@ -12,7 +13,7 @@ const Master = () => {
       {type === "delivery" && <Delivery />}
       {type === "diningout" && <Dining />}
       {type === "nightlife" && <NightLife />}
-      {/* {type === "nutri" && <Nutrition />} */}
+      {type === "nutrition" && <Nutrition />}
     </div>
     )
 }
